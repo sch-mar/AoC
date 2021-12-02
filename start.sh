8535536255abf055ed4a591394ea0f74386eb320
sch-mar <mar.schneider@gmx.net>
2021-12-02 09:44:53 +0000

    tested and modified start script

--- start.sh
+++ start.sh
@@ -18,8 +18,9 @@ path="$y/$dd/"
 for f in part1.py example input; do
 	if ! [ -f "$path$f" ]; then
 		touch "$path$f"
-		if $f = "input"; then
-			curl -b "session=53616c7465645f5ffa8137f2aa5ac770794b863a31ae75aea6733e8d7c49a1243091c45634a0b5781cc6017b651acff9" https://adventofcode.com/$y/day/$d/input>input
+		if [ "$f" = "input" ]; then
+			curl -b "session=53616c7465645f5ffa8137f2aa5ac770794b863a31ae75aea6733e8d7c49a1243091c45634a0b5781cc6017b651acff9" https://adventofcode.com/$y/day/$d/input>$path$f
 		fi
 	fi
+done
 
