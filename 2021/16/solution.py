38264ebf3e0a096349bd74b75d2d4edcab5cacf4
sch-mar <mar.schneider@gmx.net>
2022-09-03 18:39:17 +0200

    LEARN-29 #time 20m

--- 2021/16/solution.py
+++ 2021/16/solution.py
@@ -6,35 +6,45 @@ def hexbin(hex):
 def bindec(bin):
     return str(int(bin, base=2))
 
+def compute_input(input):
+    print("\ninput:", input)
+    input = hexbin(input)
+    
+    print(f'binary input: {input}')
+    
+    version_bin = input[:3]
+    version_dec = bindec(version_bin)
+    print(f'version: {version_dec} ({version_bin})')
+    
+    typeID_bin = input[3:6]
+    typeID_dec = bindec(typeID_bin)
+    print(f'type ID: {typeID_dec} ({typeID_bin})')
+    
+    LAST_GROUP = False
+    groups = 0
+    nums = []
+    
+    while LAST_GROUP == False:
+        nums.append(input[6+groups*5:11+groups*5])
+        groups += 1
+        if nums[-1][0] == '0':
+            LAST_GROUP = True
+    print(f'numbers: {nums}')
+    
+    print(f'ignored: {input[6+groups*5:]}')
+    
+    value_bin = "".join([i[1:] for i in nums])
+    value_dec = bindec(value_bin)
+    
+    print(f'packet value: {value_dec} ({value_bin})')
+    
+#################################################################
+    
 #input = [int(i) for i in open("input").readlines()]
-input = "D2FE28"
-input = hexbin(input)
+input = ["D2FE28"]
 
-print(f'binary input: {input}')
+for i in input:
+    compute_input(i)
 
-version_bin = input[:3]
-version_dec = bindec(version_bin)
-print(f'version: {version_dec} ({version_bin})')
-
-typeID_bin = input[3:6]
-typeID_dec = bindec(typeID_bin)
-print(f'type ID: {typeID_dec} ({typeID_bin})')
-
-LAST_GROUP = False
-groups = 0
-nums = []
-
-while LAST_GROUP == False:
-    nums.append(input[6+groups*5:11+groups*5])
-    groups += 1
-    if nums[-1][0] == '0':
-        LAST_GROUP = True
-print(f'numbers: {nums}')
-
-print(f'ignored: {input[6+groups*5:]}')
-
-value_bin = "".join([i[1:] for i in nums])
-value_dec = bindec(value_bin)
-
-print(f'packet value: {value_dec} ({value_bin})')
+exit()
 
