91fdf671b2321ef6dadc26881ef641eb878c680f
sch-mar <mar.schneider@gmx.net>
2022-09-04 21:01:48 +0200

    LEARN-29 #time 30m

--- 2021/16/solution.py
+++ 2021/16/solution.py
@@ -6,63 +6,69 @@ def hexbin(hex):
 def bindec(bin):
     return int(bin, base=2)
 
-def decode(input):
-
+def decode(input, totalLength=False):
+    cursor = 0
     print(f'\nparsing: {input}')
     
-    version_bin = input[:3]
+    version_bin = input[cursor:cursor+3]
+    cursor += 3
     version_dec = bindec(version_bin)
     print(f'version: {version_dec} ({version_bin})')
     
-    typeID_bin = input[3:6]
+    typeID_bin = input[cursor:cursor+3]
+    cursor += 3
     typeID_dec = bindec(typeID_bin)
     print(f'type ID: {typeID_dec} ({typeID_bin})')
 
-    if typeID_dec == 4:
+    if typeID_dec == 4: # literal value
         LAST_GROUP = False
         groups = 0
         nums = []
         
         while LAST_GROUP == False:
-            nums.append(input[6+groups*5:11+groups*5])
+            nums.append(input[cursor+groups*5:cursor+5+groups*5])
             groups += 1
             if nums[-1][0] == '0':
                 LAST_GROUP = True
         print(f'numbers: {nums}')
         
-        ignored = input[6+groups*5:]
-        print("ignored:", ignored)
+        rest = input[cursor+groups*5:]
+        print("rest:", rest)
         
         value_bin = "".join([i[1:] for i in nums])
         value_dec = bindec(value_bin)
         print(f'packet value: {value_dec} ({value_bin})')
-    else:
-        ltypeID_bin = input[6]
+    else: # operator packet
+        ltypeID_bin = input[cursor]
+        cursor += 1
         ltypeID_dec = bindec(ltypeID_bin)
         print(f'length type ID: {ltypeID_dec} ({ltypeID_bin})')
 
-        if ltypeID_bin == '0':
-            totalLengthInBits_bin = input[7:20]
+        if ltypeID_bin == '0': # subpackets by length in bits
+            totalLengthInBits_bin = input[cursor:cursor+13]
+            cursor += 13
             totalLengthInBits_dec = bindec(totalLengthInBits_bin)
             print(f'total length in bits: {totalLengthInBits_dec} ({totalLengthInBits_bin})')
 
-            subpackets = input[20:20+totalLengthInBits_dec]
+            subpackets = input[cursor:cursor+totalLengthInBits_dec]
+            cursor += totalLengthInBits_dec
             print("subpacket block:", subpackets)
 
-            ignored = input[20+totalLengthInBits_dec:]
-            print("ignored:", ignored)
+            rest = input[cursor:]
+            print("rest:", rest)
             
-            decode(subpackets)
-        else:
-            nSubpackets_bin = input[7:18]
+            decode(subpackets, totalLengthInBits_bin)
+        else: # subpackets by count
+            nSubpackets_bin = input[cursor:cursor+11]
+            cursor += 11
             nSubpackets_dec = bindec(nSubpackets_bin)
             print(f'number of sub-packets: {nSubpackets_dec} ({nSubpackets_bin})')
             
-#            for i in range(nSubpackets_dec):
-            decode(
-    if ignored != "":
-        if int(ignored, base=2) != 0:
-            decode(ignored)
+            # for i in range(nSubpackets_dec):
+
+    if rest != "":
+        if int(rest, base=2) != 0:
+            decode(rest)
     
 #################################################################
     
@@ -72,7 +78,7 @@ input = ["D2FE28", "38006F45291200", "EE00D40C823060", "8A004A801A8002F478", "62
 # for i in input:
 #     decode(i)
 
-example = 2
+example = 1
 print("input:", input[example])
 input[example] = hexbin(input[example])
 decode(input[example])
